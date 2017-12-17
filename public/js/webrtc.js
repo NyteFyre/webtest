927378a34ced0d538a3308cb9ffb23938bc4020e
Fateh Ali Khan <fazercreed@gmail.com>
2017-12-17 19:53:33 +0530

    Update webrtc.js

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -180,8 +180,8 @@ function receiveDataChannelMessage(event) {
 		fileProgress.value = fileSize;
 				
 		//Provide link to downloadable file when complete
-		if (fileSize === receivedFileSize) 
-		{
+		//if (fileSize === receivedFileSize) 
+		//{
 			var received = new window.Blob(fileBuffer);
 			fileBuffer = [];
 
@@ -199,7 +199,7 @@ function receiveDataChannelMessage(event) {
 			div.className = 'message-out';
 			div.appendChild(linkTag);
 			messageHolder.appendChild(div);
-		}
+		//}
 	}
 	//else {
 	//	appendChatMessage(event.data, 'message-out');
